a3a0e3cc4530af8a24d04fc57c41c97ef66839d7
nikomps <142419511+nikomps@users.noreply.github.com>
2024-12-30 11:25:26 +0530

    Add files via upload

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+// components/Footer.js
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2024 CoreLabs. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Header.js
@@ -0,0 +1,16 @@
+// components/Header.js
+const Header = () => {
+  return (
+    <header>
+      <img
+        src="https://media.discordapp.net/attachments/1322940025901678724/1323144018942758922/discord-avatar-1024-IPPO7.png?ex=677370c4&is=67721f44&hm=2f08b1795c59a787b191fc6d585475abadd59af1025e5d3f350facfbaef18290&"
+        alt="CoreLabs Logo"
+      />
+      <h1>CoreLabs</h1>
+      <p>Professional Discord Server Creation</p>
+      <div className="scroll-down">Scroll Down ↓</div>
+    </header>
+  );
+};
+
+export default Header;
