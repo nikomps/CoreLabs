cd131ccf76ff78933b3e7574d07eb2f797ec3652
nikomps <142419511+nikomps@users.noreply.github.com>
2024-12-30 11:26:41 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// pages/index.js
+import Header from '../components/Header';
+import Features from '../components/Features';
+import Footer from '../components/Footer';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Features />
+      <Footer />
+    </div>
+  );
+}
